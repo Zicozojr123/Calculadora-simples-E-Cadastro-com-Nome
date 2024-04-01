33a342c09e58a9f04aac2c18eda8626cc9987537
Kaio Luccas <163361249+Zicozojr123@users.noreply.github.com>
2024-04-01 18:32:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let x = parseFloat(prompt("Insert a value:"));
+let y = parseFloat(prompt("Insert a value:"));
+
+alert(
+  "Soma:" +
+    (x + y) +
+    "\n Subtração:" +
+    (x - y) +
+    "\n Multiplicação:" +
+    x * y +
+    "\n Divisão:" +
+    x / y
+);
